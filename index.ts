6d3ae92a750968946b52d17138cf4ead414c20fb
Brooks Patton <BrooksPatton@users.noreply.github.com>
2022-01-11 02:26:54 +0000

    created the basic vpc configuration that I'll be using to start with

--- index.ts
+++ index.ts
@@ -1,19 +1,28 @@
-import { Vpc } from "@pulumi/aws/ec2";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 import pulumiHelpers from "bb-pulumi-helpers-ts";
+import { createInternetGateway, createMainRouteTableAssociation, createRouteTable, createRouteTableAssociation, createSubnet, createVpc } from "bb-pulumi-helpers-ts/aws";
+import { MainRouteTableAssociation } from "@pulumi/aws/ec2";
 
-const {aws} = pulumiHelpers;
-const {createVpc} = aws;
-
-export interface ICloudConfiguration {
-  vpc: Vpc
-}
-
-async function main(): Promise<ICloudConfiguration> {
-  const vpc = await createVpc();
-
-  return {
-    vpc
+const cidrBlocks = {
+  vpc: "10.0.0.0/16",
+  subnets: {
+    public: "10.0.1.0/24",
+    private: "10.0.2.0/24"
   }
+};
+
+const availabilityZone = "a"
+
+async function main(): Promise<void> {
+  const vpc = await createVpc(cidrBlocks.vpc);
+  const internetGateway = await createInternetGateway(vpc);
+  const publicSubnet = await createSubnet(cidrBlocks.subnets.public, vpc, availabilityZone, true);
+  const privateSubnet = await createSubnet(cidrBlocks.subnets.private, vpc, availabilityZone);
+  const routeTable = await createRouteTable(vpc, internetGateway);
+  await createRouteTableAssociation(routeTable, publicSubnet);
+  await createMainRouteTableAssociation(routeTable, vpc);
 }
 
-export default main;
+main();
